25dce89eb2b7cda925678f1b5c8c40049fe78005
krkoenig <sprinkknot@gmail.com>
2015-12-23 12:49:13 -0800

    Added Bomb projectile

--- Assets/Controllers/SpellControllers/WizardSpellController.cs
+++ Assets/Controllers/SpellControllers/WizardSpellController.cs
@@ -16,7 +16,9 @@ public class WizardSpellController : SpellController
     public override void buildSpell(string axis)
     {
         if (isFireball(axis))
-                CmdSetFireball(axis);  
+            CmdSetFireball(axis);
+        if (isBomb(axis))
+            CmdSetBomb(axis);
     }
 
     public void addAir()
@@ -96,5 +98,22 @@ public class WizardSpellController : SpellController
             rightSpell = new Fireball();
     }
 
+    public bool isBomb(string axis)
+    {
+        return isPrimary((Element)WizardElement.FIRE, axis) &&
+            isSecondary((Element)WizardElement.EARTH, axis) &&
+            !isSecondary((Element)WizardElement.ARCANE, axis) &&
+            !isSecondary((Element)WizardElement.AIR, axis) &&
+            !isSecondary((Element)WizardElement.WATER, axis);
+    }
+
+    [Command]
+    private void CmdSetBomb(string axis)
+    {
+        if (axis == Constants.FIRE_LEFT)
+            leftSpell = new Bomb();
+        else if (axis == Constants.FIRE_RIGHT)
+            rightSpell = new Bomb();
+    }
 }
 
---
+++ Assets/Objects/Spells/Bomb.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class Bomb : Spell
+{
+    public override GameObject fire(Vector3 transform, Vector3 euler)
+    {
+        return (GameObject)MonoBehaviour.Instantiate(Resources.Load("Prefabs/Bomb"), transform, Quaternion.Euler(euler));
+    }
+}
